f8ba2506fe7cfe0eabe40d908dc06355b7eb3c86
Riccardo Sieve <sievericcardo@gmail.com>
2024-05-30 17:04:37 +0200

    Update HomeController.kt
    Changed request to use params and name

--- src/main/kotlin/no/uio/smolapi/controller/HomeController.kt
+++ src/main/kotlin/no/uio/smolapi/controller/HomeController.kt
@@ -13,14 +13,69 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.http.ResponseEntity
+import org.apache.jena.query.QueryExecution
+import org.apache.jena.query.QueryExecutionFactory
 
-data class QueryRequest(val query: String)
+data class QueryRequest(val parameters: List<List<String>>, val name: String? = null)
 
 @SpringBootApplication
 @RestController
 @RequestMapping("/api")
 open class HomeController {
     private val log : Logger = LoggerFactory.getLogger(this.javaClass);
+    private val fusekiHost = System.getenv("HASURA_HOST") ?: "localhost"
+    private val fusekiEndpoint = "http://$fusekiHost:3030/ds"
+
+    private fun queryGenerator(parameters : List<List<String>>, name: String?): String {
+        var graphQlData = """
+        PREFIX ast: <http://www.smolang.org/oslofjordDT#>
+        PREFIX owl: <http://www.w3.org/2002/07/owl#>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+    
+        SELECT DISTINCT
+          ?name
+          
+        """.trimIndent()
+
+        for (parameter in parameters) {
+            val param = parameter.get(0)
+            graphQlData += "  ?$param\n"
+        }
+
+       graphQlData += """
+        WHERE {
+          ?species rdfs:subClassOf ?b1 .
+          ?b1 owl:hasValue ?name ;
+              owl:onProperty ast:Name .
+        """.trimIndent()
+        
+        if (name != null) {
+            graphQlData += """
+        FILTER (?name = "$name"^^xsd:string)
+        
+        """.trimIndent()
+        }
+
+        var i = 2
+        for (parameter in parameters) {
+            val param = parameter.get(0)
+            val value = parameter.get(1)
+            graphQlData += """
+        OPTIONAL {
+          ?species rdfs:subClassOf ?b$i .
+          ?b$i owl:hasValue ?$param ;
+               owl:onProperty ast:$value .
+        }
+        
+        """.trimIndent()
+            i++
+        }
+
+        graphQlData += "}\n\n"
+
+        return graphQlData
+    }
 
     @GetMapping("/status")
     fun status(): ResponseEntity<String> {
@@ -36,7 +91,19 @@ open class HomeController {
     ])
     @PostMapping("/select")
     fun selectData(@SwaggerRequestBody(description = "Request for data retrieval") @RequestBody request: QueryRequest): ResponseEntity<String> {
-        log.info("Received query: ${request.query}")
+        log.info("Received parameters for query: ${request.parameters}")
+
+        val query = queryGenerator(request.parameters, request.name)
+        log.info("Generated query: $query")
+        log.info("Executing query to $fusekiEndpoint/query")
+
+        val q = QueryExecution.service("$fusekiEndpoint/query", query)
+        val results = q.execSelect()
+
+        while (results.hasNext()) {
+            val soln = results.nextSolution()
+            log.info(soln.toString())
+        }
 
         return ResponseEntity.ok("Query executed successfully")
     }
