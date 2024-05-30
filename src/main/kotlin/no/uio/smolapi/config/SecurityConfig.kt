f4c29be19691d561183c57faa69eab77c1e25794
Riccardo Sieve <sievericcardo@gmail.com>
2024-05-30 23:51:13 +0200

    Update SecurityConfig.kt
    Change security config to match request

--- src/main/kotlin/no/uio/smolapi/config/SecurityConfig.kt
+++ src/main/kotlin/no/uio/smolapi/config/SecurityConfig.kt
@@ -1,31 +1,32 @@
 package no.uio.smolapi.config
-//
-//import org.springframework.context.annotation.Bean
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-//import org.springframework.security.web.SecurityFilterChain
-//
-//
-//@EnableWebSecurity
-//open class SecurityConfig {
-////    @Bean
-////    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
-////        http.authorizeHttpRequests { authorizeRequests ->
-////            authorizeRequests
-//////                .requestMatchers("/public/**", "/login").permitAll()
-////                .anyRequest().permitAll()
-////        }
-////        return http.build()
-////    }
-//    @Bean
-//    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
-//        http.authorizeHttpRequests { authz ->
-//                authz
-//                    .requestMatchers("/authentication/**").permitAll()
-//                    .requestMatchers("/h2/**").permitAll()
-//                    .anyRequest().authenticated()
-//        }
-//
-//        return http.build()
-//    }
-//}
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+open class SecurityConfig {
+    @Bean
+    open fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http.authorizeHttpRequests { authz ->
+                authz
+                    .requestMatchers(
+                        "/",
+                        "/api/**",
+                        "/api-docs",
+                        "/swagger-ui.html",
+                        "/swagger-ui/**",
+                        "/v3/api-docs",
+                        "/configuration/ui",
+                        "/swagger-resources/**",
+                        "/configuration/security",
+                        "/webjars/**").permitAll()
+                    .anyRequest().authenticated()
+        }
+
+        return http.build()
+    }
+}
