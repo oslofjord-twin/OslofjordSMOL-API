747138199c0fd474adf38a58f6d9ab07b9cc24c2
Riccardo Sieve <sievericcardo@gmail.com>
2024-05-30 15:45:44 +0200

    Update HomeController.kt
    Set class to open to be Spring App and added status

--- src/main/kotlin/no/uio/smolapi/controller/HomeController.kt
+++ src/main/kotlin/no/uio/smolapi/controller/HomeController.kt
@@ -3,19 +3,30 @@ package no.uio.smolapi.controller
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.parameters.RequestBody as SwaggerRequestBody
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.http.ResponseEntity
 
 data class QueryRequest(val query: String)
 
+@SpringBootApplication
 @RestController
 @RequestMapping("/api")
-class HomeController {
+open class HomeController {
     private val log : Logger = LoggerFactory.getLogger(this.javaClass);
+
+    @GetMapping("/status")
+    fun status(): ResponseEntity<String> {
+        log.info("Service is up and running")
+        return ResponseEntity.ok("Service is up and running")
+    }
 
     @Operation(summary = "Retrieve information from the knowledge graph")
     @ApiResponses(value = [
@@ -23,8 +34,12 @@ class HomeController {
         ApiResponse(responseCode = "400", description = "Invalid query"),
         ApiResponse(responseCode = "500", description = "Internal server error")
     ])
-    @GetMapping("/select")
-    fun selectData(@RequestBody request: QueryRequest) {
+    @PostMapping("/select")
+    fun selectData(@SwaggerRequestBody(description = "Request for data retrieval") @RequestBody request: QueryRequest): ResponseEntity<String> {
+        println("Received query: ${request.query}")
         log.info("Received query: ${request.query}")
+        log.debug("Received query: ${request.query}")
+
+        return ResponseEntity.ok("Query executed successfully")
     }
 }
